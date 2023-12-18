d93ad05c9e671233a0e1539865ddd18111f84f65
Clari Loo <clarizlook.tech@gmail.com>
2023-12-18 23:49:08 +0800

    Updating services section part 3

--- src/components/Services.js
+++ src/components/Services.js
@@ -6,34 +6,34 @@ const Services = () => {
         {
             heading: 'DevOps',
             details: [
-                'Infrastructure as Code (IaC) implementation and management',
-                'Continuous Integration/Continuous Deployment (CI/CD) setup and improvements',
-                'Cloud services and migration assistance and support',
-                'Database administration',
-                'Containerization and orchestration implementation to improve scalability and resource utilization',
-                'Monitoring and logging set up and management',
-                'Infra security automation',
-                'Consultation and training'
+                '- Infrastructure as Code (IaC) implementation and management',
+                '- Continuous Integration/Continuous Deployment (CI/CD) setup and improvements',
+                '- Cloud services and migration assistance and support',
+                '- Database administration',
+                '- Containerization and orchestration implementation to improve scalability and resource utilization',
+                '- Monitoring and logging set up and management',
+                '- Infra security automation',
+                '- Consultation and training'
             ]
         },
         {
             heading: 'Web Development',
             details: [
-                'Web app development using Node.js',
-                'Full-Stack Development: Combine Node.js with front-end technologies (React)',
-                'Serverless architecture development (lambda, fargate)',
-                'Wordpress site development',
+                '- Web app development using Node.js',
+                '- Full-Stack Development: Combine Node.js with front-end technologies (React)',
+                '- Serverless architecture development (lambda, fargate)',
+                '- Wordpress site development',
             ]
         },
         {
             heading: 'Google Ads Consulting & SEO Consulting',
             details: [
-                'Keyword Research and Strategy',
-                'Ad Campaign Setup',
-                'Budget Management and forecasting',
-                'Ad Extensions implemention',
-                'Google Shopping, Mobile App pay per click, Youtube Ads management',
-                'Ads Performance Analysis and reporting'
+                '- Keyword Research and Strategy',
+                '- Ad Campaign Setup',
+                '- Budget Management and forecasting',
+                '- Ad Extensions implemention',
+                '- Google Shopping, Mobile App pay per click, Youtube Ads management',
+                '- Ads Performance Analysis and reporting'
             ]
         }
     ];
